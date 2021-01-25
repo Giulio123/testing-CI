757e5cd7f01a85016f945141b1a778dc6cf1aa71
Giulio123 <guaraldino@hotmail.it>
2021-01-25 02:18:50 +0100

    Add Spring Boot skeleton

---
+++ src/test/java/com/example/testingCI/TestingCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
